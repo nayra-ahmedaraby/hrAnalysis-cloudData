34d13e6cc321c8df6234b55796e6c93089a18d35
mernaahmed18 <114730162+mernaahmed18@users.noreply.github.com>
2026-04-30 22:29:04 +0300

    Update features.py

--- src/features.py
+++ src/features.py
@@ -1,6 +1,6 @@
 # 🟡 Member 3 (Part 1): Feature Engineering Module
 # Create polynomial features, interaction features, aggregate statistics, and normalize/scale numeric data
-   from pyspark.sql import SparkSession
+from pyspark.sql import SparkSession
 from pyspark.sql import functions as F
 from config import TARGET_ATTRITION
 
